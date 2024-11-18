fd3094d0187001947b949cb445f4a0847645d6ef
DF <_>
2024-11-19 01:04:29 +1100

    Attempting base fix

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
       "@src": resolve(__dirname, "./src"),
     },
   },
-  base: process.env.APP_ENV === "sandbox" ? "./" : "/",
+  base: process.env.APP_ENV === "sandbox" ? "/" : "/pf-shirts",
   plugins: [react()],
 });
