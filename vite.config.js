813acb7332025d2bedc9673ac6c7b65a87e8beaa
DF <_>
2024-11-19 00:55:29 +1100

    Updated home page

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,6 @@ export default defineConfig({
       "@src": resolve(__dirname, "./src"),
     },
   },
+  base: process.env.APP_ENV === "sandbox" ? "/pf-shirts/" : "/",
   plugins: [react()],
 });
